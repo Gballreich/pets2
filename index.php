a2cb75254b8f901595de931b6cd2bfc6e9f24441
Garrett Ballreich <garrett.ballreich101@gmail.com>
2024-04-22 21:19:05 -0400

    added pet-order view and route

--- index.php
+++ index.php
@@ -18,5 +18,14 @@ $f3->route('GET /', function(){
     echo $view->render('views/home.html');
 });
 
+//define a route for /order
+$f3->route('GET /order', function(){
+    //echo '<h1>THIS IS MY ORDER PAGE</h1>';
+
+    //render a view
+    $view = new Template();
+    echo $view->render('views/pet-order.html');
+});
+
 //run fat-free
 $f3->run();
